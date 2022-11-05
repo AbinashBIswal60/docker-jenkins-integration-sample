d1ed71544149079f5464fcd7b862cacc7dac717f
AbinashBIswal60 <abinashbiswal60@gmail.com>
2022-11-05 16:27:03 +0530

    Controller Added

---
+++ src/main/java/com/example/abinash/dockerjenkinsintegrationsample/DockerJenkinsIntegrationController.java
@@ -0,0 +1,14 @@
+package com.example.abinash.dockerjenkinsintegrationsample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerJenkinsIntegrationController {
+
+    @GetMapping("/")
+    public String printMessage(){
+        return "Hi Abinash, Welcome to jenkinsDemo.";
+    }
+
+}
